945196b5cf1b87c5634ed08225c30756d9b0b374
Training_H2A.03.20 <Training_h2a.03.20@accenture.com>
2019-02-11 15:32:48 +0530

    commit all the files to github

---
+++ src/test/java/myFirstJenkinJob/JenkinsJob/AppTest.java
@@ -0,0 +1,20 @@
+package myFirstJenkinJob.JenkinsJob;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
